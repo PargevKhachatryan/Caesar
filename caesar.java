c46a73bcc7c4094f7594d653fb059c304acca883
Pargev <127665233+PargevKhachatryan@users.noreply.github.com>
2023-04-10 11:47:32 +0400

    Update caesar.java

--- caesar.java
+++ caesar.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
-public class caesar{
-  public void K(){
+public class Caesar{
+  public void input(){
     Scanner sc = new Scanner(System.in);
     Scanner scNum = new Scanner(System.in);
     String str;
     int rotDir = -1;
-    caesarAlgorithm caesar = new caesarAlgorithm();
+    CaesarAlgorithm Caesar = new CaesarAlgorithm();
 
     System.out.print("Please enter the text to cipher (in english alphabet) - ");
     str = sc.nextLine();
-    caesar.str = str.toCharArray();
+    Caesar.str = str.toCharArray();
     do{
       System.out.print("\nNow enter the count of rotations - ");
-      caesar.num = scNum.nextInt();
-    } while(caesar.num <= 0);
+      Caesar.num = scNum.nextInt();
+    } while(Caesar.num <= 0);
     do{
       System.out.print("\nEnter the direction of rotation (enter 0 for LEFT || 1 for RIGTH) - ");
       rotDir = scNum.nextInt();
     } while(rotDir < 0 || rotDir > 1);
     if(rotDir == 0){
-      caesar.showLeft();
+      Caesar.showLeft();
     } else if(rotDir == 1){
-      caesar.showRight();
+      Caesar.showRight();
     }
     sc.close();
   }  
